92abf3a01321ea9595bd72d3c31ef719995cb59a
ixshelescamilla <ixshelescamilla@gmail.com>
2018-05-02 11:08:59 -0700

    fixes

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -17,7 +17,7 @@ declare var satellite;
 })
 export class AboutPage implements OnInit {
   @ViewChild('map') view1Div: ElementRef;
-  mapView: any = null;
+  view: any = null;
   data;
 
   constructor(public navCtrl: NavController, public platform: Platform, private _http: HttpClient) {
@@ -45,9 +45,9 @@ export class AboutPage implements OnInit {
       longitude = position.coords.longitude;
 
       // Center map after it has been initialized
-      if (this.mapView !== null) {
+      if (this.view !== null) {
         console.log("Centering map: " + latitude + ", " + longitude);
-        this.mapView.center = [longitude, latitude];
+        this.view.center = [longitude, latitude];
       }
 
     }, error => {
@@ -84,7 +84,7 @@ export class AboutPage implements OnInit {
         ground: 'world-elevation'
       });
 
-      this.mapView = new SceneView({
+      this.view = new SceneView({
         // create the map view at the DOM element in this component
         container: this.view1Div.nativeElement,
         map: map,
@@ -103,11 +103,6 @@ export class AboutPage implements OnInit {
         }
       });
 
-      // prevent mouse wheel zoom in or zoom out
-      this.mapView.on('mouse-wheel', (evt) => {
-        evt.stopPropagation();
-      });
-
       // create layers and tracks
       let satelliteLayer = new GraphicsLayer();
       let satelliteTracks = new GraphicsLayer();
@@ -118,65 +113,54 @@ export class AboutPage implements OnInit {
       let lines = this.data.split("\n");
       let count = (lines.length / 3).toFixed(0);
 
-      for (let i = 0; i < Number(count); i++) {
-        var commonName = lines[i * 3 + 0];
-        let line1 = lines[i * 3 + 1];
-        let line2 = lines[i * 3 + 1];
-        let time = Date.now();
+      this.getSatelliteInfo(count, lines, Graphic, satelliteLayer);
 
-        let designator = line1.substring(9, 16);
-        let launchYear = designator.substring(0, 2);
-        launchYear = (Number(launchYear) >= 57) ? "19" + launchYear : "20" +
-          launchYear;
-        let launchNum = Number(designator.substring(2, 5)).toString();
-        let noradId = Number(line1.substring(3, 7));
-        let satelliteLoc = null;
+      // prevent mouse wheel zoom in or zoom out
+      this.view.on('mouse-wheel', (evt) => {
+        evt.stopPropagation();
+      });
 
-        if (this.data) {
-          try {
-            satelliteLoc = this.getSatelliteLocation(new Date(time), line1, line2);
-          } catch (err) {
-            console.log('nooooooooooooo', err)
+      this.view.popup.on("trigger-action", function(evt) {
+        if (evt.action.id === "track" && this.view) {
+          var graphic = this.view.popup.selectedFeature;
+          var trackFeatures = [];
+
+          for (var i = 0; i < 60 * 24; i++) {
+
+            var loc = null;
+            try {
+              loc = this.getSatelliteLocation(
+                new Date(graphic.attributes.time + i * 1000 * 60),
+                graphic.attributes.line1,
+                graphic.attributes.line2
+              );
+            } catch (err) {}
+
+            if (loc !== null) {
+              trackFeatures.push([loc.x, loc.y, loc.z]);
+            }
           }
+
+          var track = new Graphic({
+            geometry: {
+              type: "polyline", // autocasts as new Polyline()
+              paths: [trackFeatures]
+            },
+            symbol: {
+              type: "line-3d", // autocasts as new LineSymbol3D()
+              symbolLayers: [{
+                type: "line", // autocasts as new LineSymbol3DLayer()
+                material: {
+                  color: [192, 192, 192, 0.5]
+                },
+                size: 3
+              }]
+            }
+          });
+
+          satelliteTracks.add(track);
         }
-
-        if (satelliteLoc !== null) {
-          var template = {
-            title: '{name}',
-            contente: 'launch number {number} of {year}',
-            actions: [{
-              title: "Show Satellite Track",
-              id: "track",
-              className: "esri-icon-globe"
-            }]
-          }
-        }
-
-        let graphic = new Graphic({
-          geometry: satelliteLoc,
-          symbol: {
-            type: "picture-marker", // autocasts as new PictureMarkerSymbol()
-            // url: "assets/imgs/satellite.png",
-            url: "assets/imgs/Star_Destroyer.png",
-            width: 48,
-            height: 48
-          },
-          attributes: {
-            name: commonName,
-            year: launchYear,
-            id: noradId,
-            number: launchNum,
-            time: time,
-            line1: line1,
-            line2: line2
-          },
-          popupTemplate: template
-        })
-
-        satelliteLayer.add(graphic);
-
-
-      }
+      });
 
     }).catch(err => {
       console.log("ArcGIS: " + err);
@@ -228,13 +212,79 @@ export class AboutPage implements OnInit {
     };
   }
 
+  getSatelliteInfo(count, lines, Graphic, satelliteLayer) {
+    for (let i = 0; i < Number(count); i++) {
+      var commonName = lines[i * 3 + 0];
+      let line1 = lines[i * 3 + 1];
+      let line2 = lines[i * 3 + 1];
+      let time = Date.now();
+
+      /*************************************************
+       * Create attributes for the International
+       * designator and Norad identifier. See the
+       * doc for details.
+       * https://www.space-track.org/documentation#/tle
+       *************************************************/
+
+      let designator = line1.substring(9, 16);
+      let launchYear = designator.substring(0, 2);
+      launchYear = (Number(launchYear) >= 57) ? "19" + launchYear : "20" +
+        launchYear;
+      let launchNum = Number(designator.substring(2, 5)).toString();
+      let noradId = Number(line1.substring(3, 7));
+      let satelliteLoc = null;
+
+      try {
+        satelliteLoc = this.getSatelliteLocation(new Date(time), line1, line2);
+      } catch (err) {
+        console.log('nooooooooooooo', err)
+      }
+
+      if (satelliteLoc !== null) {
+        var template = {
+          title: '{name}',
+          contente: 'launch number {number} of {year}',
+          actions: [{
+            title: "Show Satellite Track",
+            id: "track",
+            className: "esri-icon-globe"
+          }]
+        }
+      }
+
+      let graphic = new Graphic({
+        geometry: satelliteLoc,
+        symbol: {
+          type: "picture-marker", // autocasts as new PictureMarkerSymbol()
+          // url: "assets/imgs/satellite.png",
+          url: "assets/imgs/Star_Destroyer.png",
+          width: 48,
+          height: 48
+        },
+        attributes: {
+          name: commonName,
+          year: launchYear,
+          id: noradId,
+          number: launchNum,
+          time: time,
+          line1: line1,
+          line2: line2
+        },
+        popupTemplate: template
+      })
+
+      satelliteLayer.add(graphic);
+
+    }
+  }
+
 
   ngOnInit() {
     this._http.get('assets/lib/data.txt', { responseType: 'text' })
       .subscribe(data => {
         this.data = data;
-        if(this.data){
-          console.log('map v',this.mapView);
+        if (this.data) {
+          console.log('map v', this.view);
           this.getGeo()
         }
       });
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { File } from '@ionic-native/file';
+// import { File } from '@ionic-native/file';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,7 +10,9 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage {
   destroyersData;
 
-  constructor(public navCtrl: NavController, private file: File, private _http: HttpClient) { }
+  constructor(public navCtrl: NavController, 
+    // private file: File, 
+    private _http: HttpClient) { }
 
   getTextFile() {
     this._http.get('assets/lib/data.txt', { responseType: 'text' })
